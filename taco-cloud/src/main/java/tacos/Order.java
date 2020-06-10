0e2327a38d01578514c93afbcc787add6d7b3a36
shin <201211211yj@gmail.com>
2020-06-10 23:46:08 +0900

    [20200611-1]
    1. stereotype annotation
    스프링에서 주로 사용하는 역할 그룹을 나타내는 어노테이션.
    @Component는 스프링이 자동으로 탐색하여 생성하는 빈으로 특정 클래스를 지정하는 어노테이션
    @Repository는 @Component에서 특화된 데이터 액세스 관련 어노테이션.
    @Controller 또한 마찬가지로 지정된 클래스가 스프링 웹 MVC 컨트롤러라는것을 알려줌.
    
    2.jdbc.query는 List를 반환
      jdbc.queryForObject는 하나의 객체만을 반환

--- taco-cloud/src/main/java/tacos/Order.java
+++ taco-cloud/src/main/java/tacos/Order.java
@@ -1,11 +1,17 @@
 package tacos;
 
+import java.util.Date;
+
 import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
 
 @Data
 public class Order {
+	
+	private Long id;
+	private Date placedAt;
+	
 	@NotBlank(message="Name is required")
 	private String deliveryName;
 
